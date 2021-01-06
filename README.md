2d1ee57bb31ba88226a93ca9170156ac59d9ab2c
Carlos Alberto Duarte Pinto <alberto.duarte@eb.mil.br>
2021-01-06 06:02:28 -0300

    Make project installable

---
+++ README.md
@@ -0,0 +1,15 @@
+# open-close-mixin
+
+## About
+Easy pluggable Mixin (and decorators) for objects that may benefit from open-close dynamics.
+
+## Installing 
+You can simply install it using *pip* as follows:
+```console
+$ pip install open-close-mixin
+```
+
+## Usage
+TODO
+> ```python
+> ```
---
+++ open_close_mixin/__about__.py
@@ -0,0 +1,26 @@
+name = 'open-close-mixin'
+version = '0.9.0a'
+
+author = 'Carlos Alberto Duarte Pinto'
+author_email = 'carlos.adpinto@gmail.com'
+
+keywords = ['plugin', 'mixin', 'decorator']
+
+description = (
+    'Easy plugin for open-close class objects'
+)
+
+url = 'https://github.com/CarlosAdp/open-close-mixin'
+
+download_url = url + f'/archive/v{version}.tar.gz'
+
+classifiers = [
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Operating System :: OS Independent',
+    'Topic :: Utilities',
+    'License :: OSI Approved :: MIT License',
+    'Intended Audience :: Developers'
+]
+
+license = 'MIT'
